73dc4fa743bbc22044f244113410461b40414ee2
emmattfu <makcimdjan@gmail.com>
2020-04-26 17:27:41 +0300

    Updates

---
+++ precache-manifest.239e8c4af0ba214a489b39e8d7e988ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a0c2a86261edc5b40467d8b527c26a",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "282656f2d3f9bbc33682",
+    "url": "/react-gh-pages/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c83dfc1dd97689516fcb",
+    "url": "/react-gh-pages/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "282656f2d3f9bbc33682",
+    "url": "/react-gh-pages/static/js/2.af4e1727.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/react-gh-pages/static/js/2.af4e1727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83dfc1dd97689516fcb",
+    "url": "/react-gh-pages/static/js/main.ff70e45e.chunk.js"
+  },
+  {
+    "revision": "4766ff9c0f0c4f6ee94d",
+    "url": "/react-gh-pages/static/js/runtime-main.6b2e6a0b.js"
+  }
+]);
